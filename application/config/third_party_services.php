4c96a4551a6ec78cc5f405e9ae24dbe4a4bbd587
Harol Restrepo <hstevdev@gmail.com>
2021-11-29 00:14:36 -0500

    Third party services config file and general helper functions

---
+++ application/config/third_party_services.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$third_party_services = [
+	'mailchimp-mandrill' => [],
+	'labsmobile' => [],
+	'onesignal' => [],
+	'verifik' => [],
+	'olimpia' => [],
+	'wasabi' => [],
+	'aws' => [],
+	'azure' => [],
+	'digitalocean' => []
+];
--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -30,7 +30,97 @@ if( ! function_exists('is_logged_in') )
 	    // Get current CodeIgniter instance
 	    $CI =& get_instance();
 	    // We need to use $CI->session instead of $this->session
-	    $user = $CI->session->userdata('user_data');
+	    $user = $CI->session->userdata('logged_in');
 	    if (!isset($user)) { return false; } else { return true; }
+	}
+}
+/**
+ * Valida si es un administrador el usuario logueado
+ */
+if( ! function_exists('is_admin') )
+{
+	function is_admin() {
+	    // Get current CodeIgniter instance
+	    $CI =& get_instance();
+	    // We need to use $CI->session instead of $this->session
+	    $profile_id = $CI->session->userdata('profile_id');
+	    if (is_logged_in() && (isset($profile_id) && $profile_id == PROFILE_ADMIN)) { return TRUE; } else { return FALSE; }
+	}
+}
+
+if( ! function_exists('is_leader') )
+{
+	function is_leader() {
+	    // Get current CodeIgniter instance
+	    $CI =& get_instance();
+	    // We need to use $CI->session instead of $this->session
+	    $profile_id = $CI->session->userdata('profile_id');
+	    if (is_logged_in() && (isset($profile_id) && $profile_id == PROFILE_LEADER)) { return TRUE; } else { return FALSE; }
+	}
+}
+if( ! function_exists('is_analyst') )
+{
+	function is_analyst() {
+	    // Get current CodeIgniter instance
+	    $CI =& get_instance();
+	    // We need to use $CI->session instead of $this->session
+	    $profile_id = $CI->session->userdata('profile_id');
+	    if (is_logged_in() && (isset($profile_id) && $profile_id == PROFILE_ANALYST)) { return TRUE; } else { return FALSE; }
+	}
+}
+if( ! function_exists('is_commercial') )
+{
+	function is_commercial() {
+	    // Get current CodeIgniter instance
+	    $CI =& get_instance();
+	    // We need to use $CI->session instead of $this->session
+	    $profile_id = $CI->session->userdata('profile_id');
+	    if (is_logged_in() && (isset($profile_id) && $profile_id == PROFILE_COMMERCIAL)) { return TRUE; } else { return FALSE; }
+	}
+}
+/**
+ * Permite ver las vistas predeterminadas desde el dashboard/DB
+ * Se debe realizar una consulta a la base de datos 
+ * para comprobar el estado de los privilegios
+ */
+if( ! function_exists('can_watch') )
+{
+	function can_watch() {
+	    return FALSE;
+	}
+}
+
+if( ! function_exists('states') )
+{
+	function states() {
+		$CI =& get_instance();
+		return $CI->db->select('*')->from('states')->get()->result_object();
+	}
+}
+
+if( ! function_exists('profiles') )
+{
+	function profiles() {
+		$CI =& get_instance();
+		return $CI->db->select('*')
+		->from('users_profiles')
+		->where('state', 1)
+		->get()
+		->result_object();
+	}
+}
+
+if( ! function_exists('current_user_id') )
+{
+	function current_user_id() {
+		$CI =& get_instance();
+		return $CI->session->userdata("id");
+	}
+}
+
+if( ! function_exists('dd') )
+{
+	function dd($element) {
+		die(var_dump($element));
 	}
 }
